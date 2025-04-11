87bec21e9d3ff88556f1b762c4099c2cf91a531e
ajay-mandal <ajayrox48@gmail.com>
2025-04-11 18:22:55 +0545

    extension added for editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,6 +8,8 @@ import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from "tiptap-extension-resize-image"
 
 const Editor = () => {
   const editor = useEditor({
@@ -29,6 +31,8 @@ const Editor = () => {
       TableRow,
       TableHeader,
       TableCell,
+      Image,
+      ImageResize
     ],
     content: `
     <table>
