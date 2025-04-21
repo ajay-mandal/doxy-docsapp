359caab286696414edc37573491f8f78dbd5e820
ajay-mandal <ajayrox48@gmail.com>
2025-04-22 00:29:55 +0545

    feature: insert image

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -14,6 +14,7 @@ import {
     BoldIcon, 
     ChevronDownIcon, 
     HighlighterIcon, 
+    ImageIcon, 
     ItalicIcon, 
     Link2Icon, 
     ListTodoIcon, 
@@ -22,16 +23,105 @@ import {
     PrinterIcon, 
     Redo2Icon, 
     RemoveFormattingIcon, 
+    SearchIcon, 
     SpellCheck2Icon, 
     UnderlineIcon,
-    Undo2Icon
+    Undo2Icon,
+    UploadIcon
 } from "lucide-react";
 
 import {
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuTrigger
+    DropdownMenuTrigger,
+    DropdownMenuItem
 } from "@/components/ui/dropdown-menu";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog";
+
+const ImageButton = () => {
+    const { editor } = useEditorStore();
+    const [ imageUrl, setImageUrl ] = useState("");
+    const [ isDialogOpen, setIsDialogOpen ] = useState(false);
+    
+    const onChange = ( src: string) => {
+        editor?.chain().focus().setImage({ src }).run();
+    };
+    const onUpload = () => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+        input.onchange = (e) => {
+            const file = (e.target as HTMLInputElement).files?.[0];
+            if(file) {
+                const imageUrl = URL.createObjectURL(file);
+                onChange(imageUrl);
+            }
+        }
+        input.click();
+    };
+
+    const handleImageUrlSubmit = () => {
+        if(imageUrl) {
+            onChange(imageUrl);
+            setImageUrl("");
+            setIsDialogOpen(false);
+        }
+    }
+
+    return(
+        <>
+            <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <ImageIcon className="size-4" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem onClick={onUpload}>
+                    <UploadIcon className="size-4 mr-2" />
+                    Upload
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setIsDialogOpen(true)}>
+                    <SearchIcon className="size-4 mr-2" />
+                    Paste image url
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Insert image URL</DialogTitle>
+                </DialogHeader>
+                <Input
+                placeholder="Insert imahe URL"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                onKeyDown={(e) => {
+                    if(e.key === "Enter") {
+                        handleImageUrlSubmit();
+                    }
+                }}
+                />
+                <DialogFooter>
+                    <Button onClick={handleImageUrlSubmit}>
+                        Insert 
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    </>
+    )
+}
 
 const LinkButton = () => {
     const { editor } = useEditorStore();
@@ -343,6 +433,7 @@ const Toolbar = () => {
             <HighlightColorButton />
             <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
             <LinkButton />
+            <ImageButton />
             {sections[2].map((item)=> (
                 <ToolbarButton key={item.label} {...item} />  
             ))}
