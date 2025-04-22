8742decbf93a9856247f2f55f60e039c0cb88bef
ajay-mandal <ajayrox48@gmail.com>
2025-04-22 09:12:58 +0545

    feature: listing

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -21,6 +21,7 @@ import {
     ImageIcon, 
     ItalicIcon, 
     Link2Icon, 
+    ListIcon, 
     ListTodoIcon, 
     LucideIcon, 
     MessageSquarePlusIcon, 
@@ -48,6 +49,52 @@ import {
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog";
+
+const ListButton = () => {
+    const { editor } = useEditorStore();
+
+    const lists = [
+        {
+            label: "Bullet List",
+            icon: ListIcon,
+            isActive: () => editor?.isActive("bulletList"),
+            onClick: () => editor?.chain().focus().toggleBulletList().run()
+        },
+        {
+            label: "Ordered List",
+            icon: ListIcon,
+            isActive: () => editor?.isActive("orderedList"),
+            onClick: () => editor?.chain().focus().toggleOrderedList().run()
+        },
+    ]
+
+    return(
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <button
+            className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+            >
+                <ListIcon className="size-4" />
+            </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {lists.map(({ label, isActive, onClick, icon: Icon}) => (
+                <button
+                key={label}
+                onClick={onClick}
+                className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    isActive() && "bg-neutral-200/80"
+                )}
+                >
+                    <Icon className="size-4"/>
+                    <span className="text-sm">{label}</span>
+                </button>
+            ))}
+        </DropdownMenuContent>
+    </DropdownMenu>
+    )
+}
 
 const AlignButton = () => {
     const { editor } = useEditorStore();
@@ -495,6 +542,8 @@ const Toolbar = () => {
             <LinkButton />
             <ImageButton />
             <AlignButton />
+            { /* TODO: Line height*/}
+            <ListButton />
             {sections[2].map((item)=> (
                 <ToolbarButton key={item.label} {...item} />  
             ))}
