375b9f55026b6941e5eb99814dd7cbde2bb95ca9
ajay-mandal <ajayrox48@gmail.com>
2025-04-28 13:08:18 +0545

    feature: ruler for page margining

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import { Color } from '@tiptap/extension-color'
 import { useEditorStore } from '@/store/use-editor-store'
 import { FontSizeExtension } from "@/extensions/font-size"
 import { LineHeightExtension } from "@/extensions/line-height"
+import { Ruler } from './ruler'
 
 
 
@@ -100,6 +101,7 @@ const Editor = () => {
 
   return(
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white pring:overflow-visible'>
+      <Ruler />
         <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 print:w-full mx-auto print:min-w-0 '>
          <EditorContent editor={editor} />
         </div>
