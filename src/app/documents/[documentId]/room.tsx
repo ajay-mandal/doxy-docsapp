23bb06c23a45f1a3409deb7d39c630fbab1e23a5
Ajay Mandal <ajaymandal@Ajays-MacBook-Air.local>
2025-06-21 15:17:39 +0545

    sync

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -12,7 +12,9 @@ export function Room({ children }: { children: ReactNode }) {
 
     const params = useParams();
     return (
-        <LiveblocksProvider publicApiKey={"pk_dev_O-lNPY2TTUbl9scT3ztRVvaihQB90Fm_CL1HjL0S690ARC0rJ8qHa2WuqB4PZ_dW"}>
+        <LiveblocksProvider
+        throttle={16}
+        authEndpoint="/api/liveblocks-auth">
         <RoomProvider id={params.documentId as string}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
             {children}
